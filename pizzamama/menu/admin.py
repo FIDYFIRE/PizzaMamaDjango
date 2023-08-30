03b252bade2f96634623424ca88aadef9634ecad
fidyfire <fidyfire@gmail.com>
2023-08-30 14:36:30 +0300

    008 Ameliorer notre interface d8217administration

--- pizzamama/menu/admin.py
+++ pizzamama/menu/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 from .models import Pizza
 # Register your models here.
 
-admin.site.register(Pizza)
+class PizzaAdmin(admin.ModelAdmin):
+    list_display = ('nom', 'ingredients', 'vegetarienne', 'prix')
+    search_fields = ['nom']
+
+admin.site.register(Pizza, PizzaAdmin)
